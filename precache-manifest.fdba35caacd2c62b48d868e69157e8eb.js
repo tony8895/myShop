330ef796bf8f3c81e856d14a2ab66975f005bcf9
Tang <Tang.Tony@principal.com>
2020-07-31 09:55:03 +0800

    Updates

---
+++ precache-manifest.fdba35caacd2c62b48d868e69157e8eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cc02ed9d790686600116cd62f3e973",
+    "url": "/myShop/index.html"
+  },
+  {
+    "revision": "3d7a4be05304690915da",
+    "url": "/myShop/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "153019a9d2c74b5c84db",
+    "url": "/myShop/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3d7a4be05304690915da",
+    "url": "/myShop/static/js/2.e0d0a5ec.chunk.js"
+  },
+  {
+    "revision": "2881c4a89c3c7ad239c40a9c9582b16d",
+    "url": "/myShop/static/js/2.e0d0a5ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153019a9d2c74b5c84db",
+    "url": "/myShop/static/js/main.633935d9.chunk.js"
+  },
+  {
+    "revision": "9abc2050d5d3e548175b",
+    "url": "/myShop/static/js/runtime-main.0d3ed3ff.js"
+  }
+]);
