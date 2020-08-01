1fb9c12252826526c18f9a295e0a725c4913e785
Tang <Tang.Tony@principal.com>
2020-08-01 10:37:12 +0800

    Updates

---
+++ precache-manifest.add0317657aca7c0659a8aac402e9aea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9a35630b66e009c1c2bdb76096c10e",
+    "url": "/myShop/index.html"
+  },
+  {
+    "revision": "795426828c0a3fd38a79",
+    "url": "/myShop/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8697b2bb1da56ba16f21",
+    "url": "/myShop/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "795426828c0a3fd38a79",
+    "url": "/myShop/static/js/2.e6e3e25b.chunk.js"
+  },
+  {
+    "revision": "2881c4a89c3c7ad239c40a9c9582b16d",
+    "url": "/myShop/static/js/2.e6e3e25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8697b2bb1da56ba16f21",
+    "url": "/myShop/static/js/main.ead7aa09.chunk.js"
+  },
+  {
+    "revision": "9abc2050d5d3e548175b",
+    "url": "/myShop/static/js/runtime-main.0d3ed3ff.js"
+  },
+  {
+    "revision": "a7e8594a56aca96e1c9c362e90526145",
+    "url": "/myShop/static/media/Mask1.a7e8594a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a39f581172c10240690505d702638210.js"
+  "/myShop/precache-manifest.add0317657aca7c0659a8aac402e9aea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myShop/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
